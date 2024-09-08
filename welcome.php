f2f366848b8ed2b2d58d8822e6c9612c0ed803b7
mikaku23 <haliqksp@gmail.com>
2024-09-08 12:26:28 +0700

    tambah bg

--- welcome.php
+++ welcome.php
@@ -1,5 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        body{
+            background: url(img/1.jpg);
+            background-size:cover;
+        }
+        h3{
 
-    <marquee direction="up" scrollamount="8">
+            position:relative;
+            color:#8194ff;
+        }
+        .teks{
+            margin-bottom:200px;
+            margin-top:200px;
+            font-family:Serif;
+            padding-left:30px;
+            font-weight: bold;
+            
+        }
+        h1{
+            color:#636bb6;
+        }
+        h5{
+            color:#b3beff;
+        }
+    </style>
+     <marquee direction="up" scrollamount="5">
         <center>
         <h1>selamat datang</h1>
     </marquee>
+    <div class="teks">
+<h3>Selamat Datang di website tugas</h3>
+<h5>Ini adalah website kumpulan tugas harian</h5>
+</div>
+</body>
+</html>
+   
